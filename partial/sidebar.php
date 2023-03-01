687dc14078bfe19da855af8eaacf09051520dbdd
velesco <velescovasile40@gmail.com>
2023-03-01 13:47:16 +0200

    php

--- partial/sidebar.php
+++ partial/sidebar.php
@@ -215,7 +215,7 @@
                   <div class="according-menu"><i class="fa fa-angle-right"></i></div></a>
                   <ul class="sidebar-submenu" style="display: none;">
                      <li><a href="office.php">Office 365</a></li>
-                     <li><a href="product-page.html">Google apps</a></li>
+                     <li><a href="google.php">Google apps</a></li>
                      <li><a href="list-products.html">MailChimp</a></li>
                   </ul>
                </li>
