212f36a50954de6e3f2a4f18a8916e9d30147085
velesco <velescovasile40@gmail.com>
2023-03-03 17:15:07 +0200

    mail

--- partial/sidebar.php
+++ partial/sidebar.php
@@ -67,9 +67,9 @@
                      <div class="according-menu"><i class="fa fa-angle-right"></i></div>
                   </a>
                   <ul class="sidebar-submenu" style="display: none;">
-                     <li><a href="">Gmail</a></li>
-                     <li><a href="">Yahoo</a></li>
-                     <li><a href="">OUTLOOK</a></li>
+                     <li><a href=""><img src="assets/images/meniu/gmail.png" height="20" alt=""></a></li>
+                     <li><a href=""><img src="assets/images/meniu/yahoo.png" height="20" alt=""></a></li>
+                     <li><a href=""><img src="assets/images/meniu/outlook.png" height="20" alt=""></a></li>
                   </ul>
                </li>
                <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#">
