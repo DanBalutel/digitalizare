90dc989baf592a660d07e14c34574ca29127f952
velesco <velescovasile40@gmail.com>
2023-03-06 16:16:38 +0200

    Update footer.php

--- partial/footer.php
+++ partial/footer.php
@@ -7,19 +7,3 @@
         </div>
     </div>
 </footer>
-<div class="modal fade" id="cuiModal" tabindex="-1" aria-labelledby="tooltipmodal" style="display: block; padding-right: 0px;" aria-modal="true" role="dialog">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title">Te rugam sa introduceti CUI-ul</h5>
-        <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close" data-bs-original-title="" title=""></button>
-        </div>
-        <div class="modal-body">
-            <input type="number" class="form-control" placeholder="38911092" id="cui">   
-        </div>
-        <div class="modal-footer">
-        <button class="btn btn-secondary" type="button" data-bs-dismiss="modal" data-bs-original-title="" title="">Submit</button>
-        </div>
-    </div>
-    </div>
-</div>
