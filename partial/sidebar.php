9e8e2422e0e67b8e5cfb75fb6ffb5f9e06e66fa2
velesco <velescovasile40@gmail.com>
2023-03-07 12:26:58 +0200

    Update sidebar.php

--- partial/sidebar.php
+++ partial/sidebar.php
@@ -91,6 +91,20 @@
                      <li><a href="echipa.php">Externalizare</a></li>
                   </ul>
                </li>
+               <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#">
+                     <svg class="stroke-icon">
+                        <use href="assets/svg/icon-sprite.svg#stroke-charts"></use>
+                     </svg>
+                     <svg class="fill-icon">
+                        <use href="assets/svg/icon-sprite.svg#fill-charts"></use>
+                     </svg><span>Institutii</span>
+                     <div class="according-menu"><i class="fa fa-angle-right"></i></div>
+                     <div class="according-menu"><i class="fa fa-angle-right"></i></div>
+                  </a>
+                  <ul class="sidebar-submenu" style="display: none;">
+                     <li><a href="">Avize online </a></li>
+                  </ul>
+               </li>
                <li class="sidebar-list"><a class="sidebar-link" href="expert.php">
                      <svg class="stroke-icon">
                         <use href="assets/svg/icon-sprite.svg#stroke-user"></use>
