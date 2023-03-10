b65bc3c4d25b2f8c1d85435ef4118eb5f9049731
velesco <velescovasile40@gmail.com>
2023-03-10 11:45:12 +0200

    Update topbar.php

--- partial/topbar.php
+++ partial/topbar.php
@@ -22,17 +22,29 @@
     
     <div class="nav-left col-1 pull-right p-0">
       <ul class="nav-menus">
-        <li class="onhover-dropdown">
-          <i class="fa fa-camera"></i>
+      <li class="onhover-dropdown">
+          <div class="notification-box">
+            <svg>
+              <use href="/assets/svg/icon-sprite.svg#notification"></use>
+            </svg><span class="badge rounded-pill badge-secondary">4 </span>
+          </div>
           <div class="onhover-show-div notification-dropdown">
-              <ul>
-                  <li class="b-l-primary border-4">
-                    <a href="#">Scan Factura</a>
-                  </li>
-                  <li class="b-l-primary border-4">
-                    <a href="#">Scan DOC</a>
-                  </li>
-              </ul>
+            <h6 class="f-18 mb-0 dropdown-title">Notificări </h6>
+            <ul>
+              <li class="b-l-primary border-4">
+                <p>Delivery processing <span class="font-danger">10 min.</span></p>
+              </li>
+              <li class="b-l-success border-4">
+                <p>Order Complete<span class="font-success">1 hr</span></p>
+              </li>
+              <li class="b-l-secondary border-4">
+                <p>Tickets Generated<span class="font-secondary">3 hr</span></p>
+              </li>
+              <li class="b-l-warning border-4">
+                <p>Delivery Complete<span class="font-warning">6 hr</span></p>
+              </li>
+              <li><a class="f-w-700" href="#">Marchează citite</a></li>
+            </ul>
           </div>
         </li>
       </ul>
