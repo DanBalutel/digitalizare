1ce87b39606cec0f6466a2b041bc4b247c0cc9dd
DanBalutel96 <danbalutel@gmail.com>
2023-03-10 15:50:33 +0200

    header timer

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -853,13 +853,6 @@ function populatePage() {
     </span>
     `;
 
-        const dateNow = document.getElementById('dateNow');
-        const today = new Date();
-        const day = today.getDate().toString().padStart(2, '0');
-        const month = (today.getMonth() + 1).toString().padStart(2, '0');
-        const year = today.getFullYear().toString();
-        dateNow.innerHTML = `Log in ${day}/${month}/${year}`;
-
 
         // BLOCKS HEIGHT IN DASHBOARD
 
