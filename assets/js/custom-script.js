d31f7913eabd3042503c01a53fba2ed8dbd1073f
DanBalutel96 <danbalutel@gmail.com>
2023-03-10 14:57:13 +0200

    cui gresit

--- assets/js/custom-script.js
+++ assets/js/custom-script.js
@@ -75,19 +75,6 @@ const d = new ElementHandler();
 
 
 function renderLoadingModal(label) {
-    // const modalLoading = document.getElementById('modal-loading');
-    // const loadingLabel = document.getElementById('loadingLabel');
-    // const loadingLoader = document.getElementById('loadingLoader')
-
-    // loadingLabel.innerHTML = label;
-    // modalLoading.style = 'display: block;background: #000000a6;';
-    // loadingLoader.style = "width: 0%"
-
-    // for (let i = 0; i < 100; i++) {
-    //     setTimeout(function () {
-    //         loadingLoader.style = `width: ${i}%`;
-    //     }, i * 300);
-    // }
 
     var notify = $.notify(`<i class="fa fa-bell-o"></i>${label}`, {
         type: 'theme',
@@ -103,8 +90,6 @@ function renderLoadingModal(label) {
 
 }
 function removeLoadingModal() {
-    // const modalLoading = document.getElementById('modal-loading');
-    // modalLoading.style = 'display: none;background: #000000a6;';
     $.notifyClose();
 };
 
