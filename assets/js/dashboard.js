8031df0c7cf6a0e62d0d5782f96e0e31b51bc054
velesco <velescovasile40@gmail.com>
2023-03-10 12:50:27 +0200

    Update dashboard.js

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -973,6 +973,15 @@ function populatePage() {
     const listNrAngaj = [];
     const listAni = [];
 
+    function roundOf(n, p) {
+        const n1 = n * Math.pow(10, p + 1);
+        const n2 = Math.floor(n1 / 10);
+        if (n1 >= (n2 * 10 + 5)) {
+            return (n2 + 1) / Math.pow(10, p);
+        }
+        return n2 / Math.pow(10, p);
+    }
+    
     for (const prod in chartProdMunc) {
         roundOf(listNrAngaj.unshift(parseInt(chartProdMunc[prod].cifra_de_afaceri_neta) / parseInt(chartProdMunc[prod].numar_mediu_angajati))).toFixed(2);
         listAni.unshift(chartProdMunc[prod].an);
