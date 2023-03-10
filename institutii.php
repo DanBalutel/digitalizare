bb8a4fc7883b01fbf068e2e1635b5fa48d3dff65
velesco <velescovasile40@gmail.com>
2023-03-10 11:17:05 +0200

    Update institutii.php

--- institutii.php
+++ institutii.php
@@ -20,48 +20,48 @@
                 <div class="card">
                     <div class="card-body">
                             <h2>Ministerul Afacerilor Interne</h2>
-                            <a href="https://b.politiaromana.ro/files/pages_files/Cerere_eliberare_certificat_cazier_persoane_fizice_anexa_7-modif.22-21.06.2022.pdf" class="">
-                                <img src="https://b.politiaromana.ro/favicon.ico" width="20" height="20" alt=""> Cazier judiciar
+                            <a href="https://b.politiaromana.ro/files/pages_files/Cerere_eliberare_certificat_cazier_persoane_fizice_anexa_7-modif.22-21.06.2022.pdf" class="h5 text-uppercase">
+                                <img src="https://b.politiaromana.ro/favicon.ico" width="40" height="40" alt=""> Cazier judiciar
                             </a>
                             <br>
-                            <a href="https://b.politiaromana.ro/files/pages_files/Cerere_eliberare_certificat_integritate_anexa_1.pdf" class="">
-                                <img src="https://b.politiaromana.ro/favicon.ico" width="20" height="20" alt=""> CERTIFICAT DE INTEGRITATE COMPORTAMENTALĂ
+                            <a href="https://b.politiaromana.ro/files/pages_files/Cerere_eliberare_certificat_integritate_anexa_1.pdf" class="h5 text-uppercase">
+                                <img src="https://b.politiaromana.ro/favicon.ico" width="40" height="40" alt=""> CERTIFICAT DE INTEGRITATE COMPORTAMENTALĂ
                             </a>
                             <h2>ANAF</h2>
-                            <a href="https://static.anaf.ro/static/10/Anaf/Declaratii_R/descarcare_declaratii.htm" class="">
-                                <img src="https://www.afacerist.ro/stiri/wp-content/uploads/2020/01/ANAF_logo_transparent_PNG.png"  height="20" alt=""> Descarca Declaratii
+                            <a href="https://static.anaf.ro/static/10/Anaf/Declaratii_R/descarcare_declaratii.htm" class="h5 text-uppercase">
+                                <img src="https://www.afacerist.ro/stiri/wp-content/uploads/2020/01/ANAF_logo_transparent_PNG.png"  height="40" alt=""> Descarca Declaratii
                             </a>
                             <br>
-                            <a href="https://chat.anaf.ro/cod_tva.nsf/solicitare_tva" class="">
-                                <img src="https://www.afacerist.ro/stiri/wp-content/uploads/2020/01/ANAF_logo_transparent_PNG.png"  height="20" alt=""> Solicita Cod TVA
+                            <a href="https://chat.anaf.ro/cod_tva.nsf/solicitare_tva" class="h5 text-uppercase">
+                                <img src="https://www.afacerist.ro/stiri/wp-content/uploads/2020/01/ANAF_logo_transparent_PNG.png"  height="40" alt=""> Solicita Cod TVA
                             </a>
                             <h2>Ministerul Justitiei</h2>
-                            <a href="https://constatator.digitalizarefirme.ro/index.html" class="">
-                                <img src="https://constatator.digitalizarefirme.ro/media/content/hero-illustration.png" width="20" height="20" alt=""> Certificat constatator ONRC
+                            <a href="https://constatator.digitalizarefirme.ro/index.html" class="h5 text-uppercase">
+                                <img src="https://constatator.digitalizarefirme.ro/media/content/hero-illustration.png" width="40" height="40" alt=""> Certificat constatator ONRC
                             </a>
                             <br>
-                            <a href="https://portal.just.ro/SitePages/acasa.aspx" class="">
-                                <img src="https://portal.just.ro/_layouts/images/favicon.ico" width="20" height="20" alt=""> Portalul instantelor de judecata
+                            <a href="https://portal.just.ro/SitePages/acasa.aspx" class="h5 text-uppercase">
+                                <img src="https://portal.just.ro/_layouts/images/favicon.ico" width="40" height="40" alt=""> Portalul instantelor de judecata
                             </a>
                             <br>
-                            <a href="https://legislatie.just.ro" class="">
-                                <img src="https://legislatie.just.ro/Content/Images/Site/temanoua/favicon.ico" width="20" height="20" alt=""> PORTAL LEGISLATIV
+                            <a href="https://legislatie.just.ro" class="h5 text-uppercase">
+                                <img src="https://legislatie.just.ro/Content/Images/Site/temanoua/favicon.ico" width="40" height="40" alt=""> PORTAL LEGISLATIV
                             </a>
                             <h2>Ministerul Dezvoltarii, Lucrarilor publice si Administratiei</h2>
-                            <a href="https://epay.ancpi.ro/epay/SelectProd.action?prodId=419" class="">
-                                <img src="https://citypressconstanta.ro/wp-content/uploads/2020/06/ancpi.png" width="20" height="20" alt=""> EXTRAS DIN PLANUL CADASTRAL, PE ORTOFOTOPLAN
+                            <a href="https://epay.ancpi.ro/epay/SelectProd.action?prodId=419" class="h5 text-uppercase">
+                                <img src="https://www.finantare.ro/wp-content/uploads/bfi_thumb/phpThumb_generated_thumbnailjpg-4-oa9v7otd3nfzwsyl9cce1rxpwevxlg57uid4fiq6yg.jpg" width="40" height="40" alt=""> EXTRAS DIN PLANUL CADASTRAL, PE ORTOFOTOPLAN
                             </a>
                             <h2>Ministerul Finantelor</h2>
-                            <a href="https://taxatimbru.ro/calculator-taxa.html" class="">
-                                <img src="https://taxatimbru.ro/assets/img/favicon/favicon-32x32.png" width="20" height="20" alt=""> Calculator taxă timbru judiciar online
+                            <a href="https://taxatimbru.ro/calculator-taxa.html" class="h5 text-uppercase">
+                                <img src="https://taxatimbru.ro/assets/img/favicon/favicon-32x32.png" width="40" height="40" alt=""> Calculator taxă timbru judiciar online
                             </a>
                             <br>
-                            <a href="https://taxatimbru.ro/plateste-taxa-timbru-persoana-fizica.html" class="">
-                                <img src="https://taxatimbru.ro/assets/img/favicon/favicon-32x32.png" width="20" height="20" alt=""> Plateste taxă timbru judiciar online persoana fizica
+                            <a href="https://taxatimbru.ro/plateste-taxa-timbru-persoana-fizica.html" class="h5 text-uppercase">
+                                <img src="https://taxatimbru.ro/assets/img/favicon/favicon-32x32.png" width="40" height="40" alt=""> Plateste taxă timbru judiciar online persoana fizica
                             </a>
                             <br>
-                            <a href="https://taxatimbru.ro/plateste-taxa-timbru-persoana-juridica.html" class="">
-                                <img src="https://taxatimbru.ro/assets/img/favicon/favicon-32x32.png" width="20" height="20" alt=""> Plateste taxă timbru judiciar online persoana judiciar
+                            <a href="https://taxatimbru.ro/plateste-taxa-timbru-persoana-juridica.html" class="h5 text-uppercase">
+                                <img src="https://taxatimbru.ro/assets/img/favicon/favicon-32x32.png" width="40" height="40" alt=""> Plateste taxă timbru judiciar online persoana judiciar
                             </a>
                         </div>
                     </div>
