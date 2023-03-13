2827c1a43316504a2afc2287bc828e9542c0a900
velesco <velescovasile40@gmail.com>
2023-03-13 11:44:49 +0200

    Update monica.php

--- monica.php
+++ monica.php
@@ -46,7 +46,7 @@
                           <li>
                             <div class="message my-message"><img class="rounded-circle float-start chat-user-img img-30"
                                 src="../assets/images/user/3.png" alt="">
-                              Salut, adreseaza-mi o intrebare, sau click pe o intrebare de mai sus.
+                              Bună, întreabă-mă orice!
                             </div>
                           </li>
 
