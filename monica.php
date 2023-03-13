ab08058ce545f2bc694f6b1ed58babaf2b6baf64
velesco <velescovasile40@gmail.com>
2023-03-13 11:45:34 +0200

    Update monica.php

--- monica.php
+++ monica.php
@@ -73,7 +73,7 @@
           </div>
         </div>
       </div>
-      <h3>Sau da apasa pe exemplele de mai jos</h3>
+      <h3>Sau apasa pe exemplele de mai jos</h3>
         <div class="row">
           <div class="col-md-4">
             <a style="cursor: pointer;">
