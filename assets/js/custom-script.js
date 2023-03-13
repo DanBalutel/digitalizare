cb9333af1d730faca3257797c6c01077a2240d2a
Dan Balutel <danbalutel@gmail.com>
2023-03-13 13:34:23 +0200

    smsh

--- assets/js/custom-script.js
+++ assets/js/custom-script.js
@@ -120,3 +120,4 @@ const day = today.getDate().toString().padStart(2, '0');
 const month = (today.getMonth() + 1).toString().padStart(2, '0');
 const year = today.getFullYear().toString();
 dateNow.innerHTML = `Log in ${day}/${month}/${year}`;
+
