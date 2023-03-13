d2f615683a7dd7ea244a5cea2faa951846eeced0
velesco <velescovasile40@gmail.com>
2023-03-13 13:25:33 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,7 +32,16 @@
             }         
         });   
       }
+      
+      $(document).ready(function() {
+        var fileName = $("#fileInput").val();
 
+        if(fileName) { // returns true if the string is not empty
+            alert(fileName + " was selected");
+        } else { // no file was selected
+            alert("no file selected");
+        }
+    });
     /*----------------------------------------
      passward show hide
      ----------------------------------------*/
