12cc35d1ac57beafd8c16ce32d325db2689b9651
velesco <velescovasile40@gmail.com>
2023-03-14 17:08:10 +0200

    Update user-profile.php

--- user-profile.php
+++ user-profile.php
@@ -133,19 +133,19 @@
                         <h2><i data-feather="clock"></i> 5h 10m</h2>
                         <hr>
                         <div class="row">
-                          <div class="col-sm-4">
+                          <div class="col-sm-6">
                             <h4>Google</h4>
                             <p>10 minute</p>
                           </div>
-                          <div class="col-sm-4">
+                          <div class="col-sm-6">
                             <h4>Facebook</h4>
                             <p>20 minute</p>
                           </div>
-                          <div class="col-sm-4">
+                          <div class="col-sm-6">
                             <h4>CRM</h4>
                             <p>50 minute</p>
                           </div>
-                          <div class="col-sm-4">
+                          <div class="col-sm-6">
                             <h4>ERP</h4>
                             <p>20 minute</p>
                           </div>
