aa58ed4d4ae7bf6806fca1f827655a756eae1c1a
velesco <velescovasile40@gmail.com>
2023-03-15 14:29:08 +0200

    Update banci.php

--- banci.php
+++ banci.php
@@ -24,7 +24,7 @@
                 <div class="card balance-box height-equal-2">
                     <div class="card-body d-flex align-items-center justify-content-center">
                         <div class="balance-profile">
-                        <div style="">
+                        <div style="padding: 20px;background: aliceblue;border-radius: 20px;">
                             <img src="assets/images/banci/brd.png" width="200" alt="user vector">
                         </div>
                         <span class="f-light d-block mt-2">Soldul </span>
@@ -51,6 +51,251 @@
                             </div>
                             </li>
                         </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xxl-3 col-xl-4 col-sm-6 box-col-6"> 
+                <div class="card balance-box height-equal-2">
+                    <div class="card-body d-flex align-items-center justify-content-center">
+                        <div class="balance-profile">
+                        <div style="padding: 20px;background: aliceblue;border-radius: 20px;">
+                            <img src="assets/images/banci/bcr.png" width="200" alt="user vector">
+                        </div>
+                        <span class="f-light d-block mt-2">Soldul </span>
+                        <h5 class="mt-1">1,987,500.90 LEI</h5>
+                        <ul> 
+                            <li>
+                            <div class="balance-item danger"> 
+                                <div class="balance-icon-wrap"> 
+                                <div class="balance-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-down-right"><line x1="7" y1="7" x2="17" y2="17"></line><polyline points="17 7 17 17 7 17"></polyline></svg></div>
+                                </div>
+                                <div> <span class="f-12 f-light">Cheltuieli  </span>
+                                <h5>78.8K</h5><span class="badge badge-light-danger rounded-pill">-11.67%</span>
+                                </div>
+                            </div>
+                            </li>
+                            <li>
+                            <div class="balance-item success">
+                                <div class="balance-icon-wrap"> 
+                                <div class="balance-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up-right"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg></div>
+                                </div>
+                                <div> <span class="f-12 f-light">Venituri</span>
+                                <h5>19.7K</h5><span class="badge badge-light-success rounded-pill">+10.67%</span>
+                                </div>
+                            </div>
+                            </li>
+                        </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xxl-3 col-xl-4 col-sm-6 box-col-6"> 
+                    <div class="card balance-box height-equal-2">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <div class="balance-profile">
+                            <div style="padding: 20px;background: aliceblue;border-radius: 20px;">
+                                <img src="assets/images/banci/bt.png" width="200" alt="user vector">
+                            </div>
+                            <span class="f-light d-block mt-2">Soldul </span>
+                            <h5 class="mt-1">1,987,500.90 LEI</h5>
+                            <ul> 
+                                <li>
+                                <div class="balance-item danger"> 
+                                    <div class="balance-icon-wrap"> 
+                                    <div class="balance-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-down-right"><line x1="7" y1="7" x2="17" y2="17"></line><polyline points="17 7 17 17 7 17"></polyline></svg></div>
+                                    </div>
+                                    <div> <span class="f-12 f-light">Cheltuieli  </span>
+                                    <h5>78.8K</h5><span class="badge badge-light-danger rounded-pill">-11.67%</span>
+                                    </div>
+                                </div>
+                                </li>
+                                <li>
+                                <div class="balance-item success">
+                                    <div class="balance-icon-wrap"> 
+                                    <div class="balance-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up-right"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg></div>
+                                    </div>
+                                    <div> <span class="f-12 f-light">Venituri</span>
+                                    <h5>19.7K</h5><span class="badge badge-light-success rounded-pill">+10.67%</span>
+                                    </div>
+                                </div>
+                                </li>
+                            </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xxl-3 col-xl-4 col-sm-6 box-col-6"> 
+                    <div class="card balance-box height-equal-2">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <div class="balance-profile">
+                            <div style="padding: 20px;background: aliceblue;border-radius: 20px;">
+                                <img src="assets/images/banci/cec.png" width="200" alt="user vector">
+                            </div>
+                            <span class="f-light d-block mt-2">Soldul </span>
+                            <h5 class="mt-1">1,987,500.90 LEI</h5>
+                            <ul> 
+                                <li>
+                                <div class="balance-item danger"> 
+                                    <div class="balance-icon-wrap"> 
+                                    <div class="balance-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-down-right"><line x1="7" y1="7" x2="17" y2="17"></line><polyline points="17 7 17 17 7 17"></polyline></svg></div>
+                                    </div>
+                                    <div> <span class="f-12 f-light">Cheltuieli  </span>
+                                    <h5>78.8K</h5><span class="badge badge-light-danger rounded-pill">-11.67%</span>
+                                    </div>
+                                </div>
+                                </li>
+                                <li>
+                                <div class="balance-item success">
+                                    <div class="balance-icon-wrap"> 
+                                    <div class="balance-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up-right"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg></div>
+                                    </div>
+                                    <div> <span class="f-12 f-light">Venituri</span>
+                                    <h5>19.7K</h5><span class="badge badge-light-success rounded-pill">+10.67%</span>
+                                    </div>
+                                </div>
+                                </li>
+                            </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xxl-3 col-xl-4 col-sm-6 box-col-6"> 
+                    <div class="card balance-box height-equal-2">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <div class="balance-profile">
+                            <div style="padding: 20px;background: aliceblue;border-radius: 20px;">
+                                <img src="assets/images/banci/ing.png" width="200" alt="user vector">
+                            </div>
+                            <span class="f-light d-block mt-2">Soldul </span>
+                            <h5 class="mt-1">1,987,500.90 LEI</h5>
+                            <ul> 
+                                <li>
+                                <div class="balance-item danger"> 
+                                    <div class="balance-icon-wrap"> 
+                                    <div class="balance-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-down-right"><line x1="7" y1="7" x2="17" y2="17"></line><polyline points="17 7 17 17 7 17"></polyline></svg></div>
+                                    </div>
+                                    <div> <span class="f-12 f-light">Cheltuieli  </span>
+                                    <h5>78.8K</h5><span class="badge badge-light-danger rounded-pill">-11.67%</span>
+                                    </div>
+                                </div>
+                                </li>
+                                <li>
+                                <div class="balance-item success">
+                                    <div class="balance-icon-wrap"> 
+                                    <div class="balance-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up-right"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg></div>
+                                    </div>
+                                    <div> <span class="f-12 f-light">Venituri</span>
+                                    <h5>19.7K</h5><span class="badge badge-light-success rounded-pill">+10.67%</span>
+                                    </div>
+                                </div>
+                                </li>
+                            </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xxl-3 col-xl-4 col-sm-6 box-col-6"> 
+                    <div class="card balance-box height-equal-2">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <div class="balance-profile">
+                            <div style="padding: 20px;background: aliceblue;border-radius: 20px;">
+                                <img src="assets/images/banci/unicredit.png" width="200" alt="user vector">
+                            </div>
+                            <span class="f-light d-block mt-2">Soldul </span>
+                            <h5 class="mt-1">1,987,500.90 LEI</h5>
+                            <ul> 
+                                <li>
+                                <div class="balance-item danger"> 
+                                    <div class="balance-icon-wrap"> 
+                                    <div class="balance-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-down-right"><line x1="7" y1="7" x2="17" y2="17"></line><polyline points="17 7 17 17 7 17"></polyline></svg></div>
+                                    </div>
+                                    <div> <span class="f-12 f-light">Cheltuieli  </span>
+                                    <h5>78.8K</h5><span class="badge badge-light-danger rounded-pill">-11.67%</span>
+                                    </div>
+                                </div>
+                                </li>
+                                <li>
+                                <div class="balance-item success">
+                                    <div class="balance-icon-wrap"> 
+                                    <div class="balance-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up-right"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg></div>
+                                    </div>
+                                    <div> <span class="f-12 f-light">Venituri</span>
+                                    <h5>19.7K</h5><span class="badge badge-light-success rounded-pill">+10.67%</span>
+                                    </div>
+                                </div>
+                                </li>
+                            </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xxl-3 col-xl-4 col-sm-6 box-col-6"> 
+                    <div class="card balance-box height-equal-2">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <div class="balance-profile">
+                            <div style="padding: 20px;background: aliceblue;border-radius: 20px;">
+                                <img src="assets/images/banci/raiffeisen.png" width="200" alt="user vector">
+                            </div>
+                            <span class="f-light d-block mt-2">Soldul </span>
+                            <h5 class="mt-1">1,987,500.90 LEI</h5>
+                            <ul> 
+                                <li>
+                                <div class="balance-item danger"> 
+                                    <div class="balance-icon-wrap"> 
+                                    <div class="balance-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-down-right"><line x1="7" y1="7" x2="17" y2="17"></line><polyline points="17 7 17 17 7 17"></polyline></svg></div>
+                                    </div>
+                                    <div> <span class="f-12 f-light">Cheltuieli  </span>
+                                    <h5>78.8K</h5><span class="badge badge-light-danger rounded-pill">-11.67%</span>
+                                    </div>
+                                </div>
+                                </li>
+                                <li>
+                                <div class="balance-item success">
+                                    <div class="balance-icon-wrap"> 
+                                    <div class="balance-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up-right"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg></div>
+                                    </div>
+                                    <div> <span class="f-12 f-light">Venituri</span>
+                                    <h5>19.7K</h5><span class="badge badge-light-success rounded-pill">+10.67%</span>
+                                    </div>
+                                </div>
+                                </li>
+                            </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xxl-3 col-xl-4 col-sm-6 box-col-6"> 
+                    <div class="card balance-box height-equal-2">
+                        <div class="card-body d-flex align-items-center justify-content-center">
+                            <div class="balance-profile">
+                            <div style="padding: 20px;background: aliceblue;border-radius: 20px;">
+                                <img src="assets/images/banci/revolut.png" width="200" alt="user vector">
+                            </div>
+                            <span class="f-light d-block mt-2">Soldul </span>
+                            <h5 class="mt-1">1,987,500.90 LEI</h5>
+                            <ul> 
+                                <li>
+                                <div class="balance-item danger"> 
+                                    <div class="balance-icon-wrap"> 
+                                    <div class="balance-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-down-right"><line x1="7" y1="7" x2="17" y2="17"></line><polyline points="17 7 17 17 7 17"></polyline></svg></div>
+                                    </div>
+                                    <div> <span class="f-12 f-light">Cheltuieli  </span>
+                                    <h5>78.8K</h5><span class="badge badge-light-danger rounded-pill">-11.67%</span>
+                                    </div>
+                                </div>
+                                </li>
+                                <li>
+                                <div class="balance-item success">
+                                    <div class="balance-icon-wrap"> 
+                                    <div class="balance-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up-right"><line x1="7" y1="17" x2="17" y2="7"></line><polyline points="7 7 17 7 17 17"></polyline></svg></div>
+                                    </div>
+                                    <div> <span class="f-12 f-light">Venituri</span>
+                                    <h5>19.7K</h5><span class="badge badge-light-success rounded-pill">+10.67%</span>
+                                    </div>
+                                </div>
+                                </li>
+                            </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
