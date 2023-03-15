22072ffadcc8d12ba648ce0abe11abf742f5bd0e
velesco <velescovasile40@gmail.com>
2023-03-15 16:09:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -141,7 +141,7 @@
               </div>
             </div>
           </div>
-          <div class="col-md-6 col-sm-12 box-col-6"> 
+          <div class="col-md-4 col-sm-12 box-col-6"> 
               <div class="card balance-box" style="min-height: 20rem;">
                   <div class="card-body d-flex align-items-center justify-content-center">
                       <div class="balance-profile">
@@ -176,7 +176,7 @@
                   </div>
               </div>
           </div>
-          <div class="col-md-6 col-sm-12 box-col-6"> 
+          <div class="col-md-4 col-sm-12 box-col-6"> 
               <div class="card balance-box" style="min-height: 20rem;">
                   <div class="card-body d-flex align-items-center justify-content-center">
                       <div class="balance-profile">
@@ -210,6 +210,12 @@
                       </div>
                   </div>
               </div>
+          </div>
+          <div class="col-md-4 col-sm-12 box-col-6"> 
+          <a class="weatherwidget-io" href="https://forecast7.com/en/40d71n74d01/new-york/" data-label_1="NEW YORK" data-label_2="WEATHER" data-theme="dark" >NEW YORK WEATHER</a>
+<script>
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+</script>
           </div>
 
           <div class="col-md-6 col-xs-12">
