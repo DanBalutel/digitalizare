2abbc19187c9e6407a9242af1c9076aba4f635f5
velesco <velescovasile40@gmail.com>
2023-03-16 12:34:53 +0200

    Update sidebar.php

--- partial/sidebar.php
+++ partial/sidebar.php
@@ -238,8 +238,8 @@
                      <div class="according-menu"><i class="fa fa-angle-right"></i></div>
                   </a>
                   <ul class="sidebar-submenu" style="display: none;">
-                     <li><a href="#"><img src="assets/images/meniu/e-factura.png" height="50" alt=""></a></li>
-                     <li><a href="#"><img src="assets/images/meniu/smartbill.png" height="50" alt=""></a></li>
+                     <li><a href="#"><img src="assets/images/meniu/e-factura.png" height="20" alt=""></a></li>
+                     <li><a href="#"><img src="assets/images/meniu/smartbill.png" height="20" alt=""></a></li>
                      <li><a href="#"><img src="assets/images/meniu/software.png" height="20" alt=""></a></li>
                      <li><a href="#">Emite oferta (ProForma)</a></li>
                      <li><a href="#">Incasari</a></li>
