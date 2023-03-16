fe74a2612e3de6c3adaeb3437cc753497c2f099b
velesco <velescovasile40@gmail.com>
2023-03-16 13:46:08 +0200

    Update user-profile.php

--- user-profile.php
+++ user-profile.php
@@ -243,7 +243,7 @@
             start: '2023-03-16T10:30:00',
             end: '2023-03-22T11:30:00',
             display: 'background',
-            backgroundColor: yellow,
+            backgroundColor: '#0000ff',
             extendedProps: {
               department: 'BioChemistry'
             },
