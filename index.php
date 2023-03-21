ea5802dd41c40bdcd7c11b367206981b71ab4bea
velesco <velescovasile40@gmail.com>
2023-03-21 13:10:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -243,28 +243,45 @@
           <div class="col-xl-12 col-md-12 col-sm-12 box-col-12">
             <div class="card balance-box">
               <div class="card-body df-flex align-items-center justify-content-center">
-                <script>
-                  (function(d, s, id) {
-                    if (d.getElementById(id)) {
-                      if (window.__TOMORROW__) {
-                        window.__TOMORROW__.renderWidget();
-                      }
-                      return;
-                    }
-                    const fjs = d.getElementsByTagName(s)[0];
-                    const js = d.createElement(s);
-                    js.id = id;
-                    js.src = "https://www.tomorrow.io/v1/widget/sdk/sdk.bundle.min.js";
+              <script>
+        (function(d, s, id) {
+            if (d.getElementById(id)) {
+                if (window.__TOMORROW__) {
+                    window.__TOMORROW__.renderWidget();
+                }
+                return;
+            }
+            const fjs = d.getElementsByTagName(s)[0];
+            const js = d.createElement(s);
+            js.id = id;
+            js.src = "https://www.tomorrow.io/v1/widget/sdk/sdk.bundle.min.js";
 
-                    fjs.parentNode.insertBefore(js, fjs);
-                  })(document, 'script', 'tomorrow-sdk');
-                </script>
+            fjs.parentNode.insertBefore(js, fjs);
+        })(document, 'script', 'tomorrow-sdk');
+        </script>
 
-                <div class="tomorrow" data-location-id="" data-language="RO" data-unit-system="METRIC" data-skin="dark" data-widget-type="aqiMini" style="padding-bottom:22px;position:relative;">
-                  <a href="https://www.tomorrow.io/weather-api/" rel="nofollow noopener noreferrer" target="_blank" style="position: absolute; bottom: 0; transform: translateX(-50%); left: 50%;">
-                    <img alt="Powered by the Tomorrow.io Weather API" src="https://weather-website-client.tomorrow.io/img/powered-by.svg" width="250" height="18" />
-                  </a>
-                </div>
+        <div class="tomorrow"
+           data-location-id="096311"
+           data-language="RO"
+           data-unit-system="METRIC"
+           data-skin="light"
+           data-widget-type="upcoming"
+           style="padding-bottom:22px;position:relative;"
+        >
+          <a
+            href="https://www.tomorrow.io/weather-api/"
+            rel="nofollow noopener noreferrer"
+            target="_blank"
+            style="position: absolute; bottom: 0; transform: translateX(-50%); left: 50%;"
+          >
+            <img
+              alt="Powered by the Tomorrow.io Weather API"
+              src="https://weather-website-client.tomorrow.io/img/powered-by.svg"
+              width="250"
+              height="18"
+            />
+          </a>
+        </div>
               </div>
             </div>
           </div>
