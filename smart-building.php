9f9c78e94d661ca9dd8325850c1b2e5968e50754
DanBalutel <danbalutel@gmail.com>
2023-03-24 13:29:27 +0200

    iot image width

--- smart-building.php
+++ smart-building.php
@@ -57,7 +57,7 @@
           <div class="col-sm-12">
             <div class="card small-widget mb-sm-0">
               <div class="card-body success d-flex align-items-center justify-content-center">
-                <img class="w-100" src="assets/images/only1/iot1.jpeg" alt="">
+                <img id="iotImage1" src="assets/images/only1/iot1.jpeg" alt="">
               </div>
             </div>
           </div>
