020d5dbeefee3a4461f3942fbe0bfc38b675b255
DanBalutel <danbalutel@gmail.com>
2023-03-31 13:11:21 +0300

    modificari depozite

--- depozite.php
+++ depozite.php
@@ -2,136 +2,166 @@
 <?php include('partial/loader.php') ?>
 <!-- page-wrapper Start-->
 <div class="page-wrapper compact-wrapper" id="pageWrapper">
-  <!-- Page Header Start-->
-  <?php include('partial/topbar.php') ?>
-  <!-- Page Header Ends-->
-  <!-- Page Body Start-->
-  <div class="page-body-wrapper">
-    <!-- Page Sidebar Start-->
-    <?php include('partial/sidebar.php') ?>
-    <!-- Page Sidebar Ends-->
-    <div class="page-body">
-    
-      <?php 
-        $title = 'Fonduri UE';
-        include('partial/breadcrumb.php') 
-      ?>
-      <!-- Container-fluid starts-->
-      
-<div class="container-fluid">
-    <div class="card">
-        <div class="card-body">
+    <!-- Page Header Start-->
+    <?php include('partial/topbar.php') ?>
+    <!-- Page Header Ends-->
+    <!-- Page Body Start-->
+    <div class="page-body-wrapper">
+        <!-- Page Sidebar Start-->
+        <?php include('partial/sidebar.php') ?>
+        <!-- Page Sidebar Ends-->
+        <div class="page-body">
 
+            <?php
+            $title = 'Fonduri UE';
+            include('partial/breadcrumb.php')
+            ?>
+            <!-- Container-fluid starts-->
 
-            <div class="row">
-                <div class="col-md-3">
+            <div class="container-fluid">
 
-                    <div class="card-title"><h2>Informatii depozite</h2></div>
-                    <hr>
-                    <label for="">Suma depozit</label>
-                    <input type="number" class="form-control" placeholder="2000">
-                    <br>
-                    <label for="">Perioada</label>
-                    <select name="" id="" class="form-select">
-                        <option value="">6 luni</option>
-                        <option value="">1 an</option>
-                        <option value="">3 ani</option>
-                        <option value="">4 ani</option>
-                        <option value="">5 ani</option>
-                        <option value="">6 ani</option>
-                        <option value="">7 ani</option>
-                        <option value="">8 ani</option>
-                        <option value="">9 ani</option>
-                        <option value="">10 ani</option>
-                        <option value="">11 ani</option>
-                        <option value="">12 ani</option>
-                        <option value="">13 ani</option>
-                        <option value="">14 ani</option>
-                        <option value="">15 ani</option>
-                    </select>
+                <div class="row">
+                    <h3 style="color:#00CCFF">Avem parteneriat cu o societate de Brokeraj care va face UPDATE saptamanal la
+                        dobanzile si plafoanele de acordare a Depozitelor.
+                    </h3>
+                    <h4>
+                        Urmeaza sa includem si ofertele de Titluri de Stat de la Guvern
+                    </h4>
+                    <h4>
+                        <b><u>Fonduri UE:</u></b> Daca se vor completa toate CAEN-urile autorizate ale companiei si
+                        aplicatia poate accesa toate informatiile financiare in timp real:
+                    </h4>
+                    <h4 class="pb-3" style="color:#00CCFF">
+                        Veti fi notificat pentru toate masurile de finantare pe care puteti sa aplicati si punctajul pe
+                        care il obtineti - ASTFEL - Nu veti mai pierde nici o oportunitate de investitii
+                    </h4>
                 </div>
-                <div class="col-md-9">
-                    
-                    <div class="card-title"><h2>Oferte Bancare</h2></div>
-                    <hr>
-                    <div class="row align-items-center">
-                        <div class="col-md-3">
-                        <p>
-                            <a target="_blank" href="https://tbibank.ro"><img src="https://mail.google.com/mail/u/0?ui=2&amp;ik=3b3961d652&amp;attid=0.1&amp;permmsgid=msg-f:1758631920665833221&amp;th=1867ea6a09f32f05&amp;view=fimg&amp;fur=ip&amp;sz=s0-l75-ft&amp;attbid=ANGjdJ_4Srh9zlgpYh5c4RlAKp6oqY1Y_2X6knkCcoAJ6QTK6JQjy1BIT5f57eap--zsKCt-X17JGnxJXzPhYmnVQ2HRtZURHIOOaWF2EN8IhIXJnONYO4TuUOB4JeY&amp;disp=emb&amp;realattid=ii_leh6yu1r0" width="180" alt=""></a>
-                            </p><h4>DEPOZIT</h4>
-                        <p></p>
+
+
+
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-3">
+
+                                <div class="card-title">
+                                    <h2>Informatii depozite</h2>
+                                </div>
+                                <hr>
+                                <label for="">Suma depozit</label>
+                                <input type="number" class="form-control" placeholder="2000">
+                                <br>
+                                <label for="">Perioada</label>
+                                <select name="" id="" class="form-select">
+                                    <option value="">6 luni</option>
+                                    <option value="">1 an</option>
+                                    <option value="">3 ani</option>
+                                    <option value="">4 ani</option>
+                                    <option value="">5 ani</option>
+                                    <option value="">6 ani</option>
+                                    <option value="">7 ani</option>
+                                    <option value="">8 ani</option>
+                                    <option value="">9 ani</option>
+                                    <option value="">10 ani</option>
+                                    <option value="">11 ani</option>
+                                    <option value="">12 ani</option>
+                                    <option value="">13 ani</option>
+                                    <option value="">14 ani</option>
+                                    <option value="">15 ani</option>
+                                </select>
+                            </div>
+                            <div class="col-md-9">
+
+                                <div class="card-title">
+                                    <h2>Oferte Bancare</h2>
+                                </div>
+                                <hr>
+                                <div class="row align-items-center">
+                                    <div class="col-md-3">
+                                        <p>
+                                            <a target="_blank" href="https://tbibank.ro"><img src="https://mail.google.com/mail/u/0?ui=2&amp;ik=3b3961d652&amp;attid=0.1&amp;permmsgid=msg-f:1758631920665833221&amp;th=1867ea6a09f32f05&amp;view=fimg&amp;fur=ip&amp;sz=s0-l75-ft&amp;attbid=ANGjdJ_4Srh9zlgpYh5c4RlAKp6oqY1Y_2X6knkCcoAJ6QTK6JQjy1BIT5f57eap--zsKCt-X17JGnxJXzPhYmnVQ2HRtZURHIOOaWF2EN8IhIXJnONYO4TuUOB4JeY&amp;disp=emb&amp;realattid=ii_leh6yu1r0" width="180" alt=""></a>
+                                        </p>
+                                        <h4>DEPOZIT</h4>
+                                        <p></p>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <p>
+                                        </p>
+                                        <h5>DAE</h5>
+                                        <h3 class="text-secondary">9.30%</h3>
+                                        <p></p>
+                                    </div>
+                                    <div class="col-md-3 ">
+                                        <a target="_blank" href="https://imfs.ro/simulator-credit/?tip=imobiliar" class="btn btn-warning align-middle">Negociaza dobanda</a>
+                                    </div>
+                                    <hr>
+                                    <div class="col-md-3">
+                                        <p>
+                                            <a target="_blank" href="https://www.otpbank.ro/ro"><img src="https://imfs.ro/wp-content/uploads/2021/04/OTP_Bank_Rom%C3%A2nia.svg.png" width="180" alt=""></a>
+                                        </p>
+                                        <h4>DEPOZIT</h4>
+                                        <p></p>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <p>
+                                        </p>
+                                        <h5>DAE</h5>
+                                        <h3 class="text-secondary">9.24%</h3>
+                                        <p></p>
+                                    </div>
+                                    <div class="col-md-3 ">
+                                        <a target="_blank" href="https://imfs.ro/simulator-credit/?tip=imobiliar" class="btn btn-warning align-middle">Negociaza dobanda</a>
+                                    </div>
+                                    <hr>
+                                    <div class="col-md-3">
+                                        <p>
+                                            <a target="_blank" href="https://www.brd.ro"><img src="https://imfs.ro/wp-content/uploads/2021/04/logo-mic.png" width="180" alt=""></a>
+                                        </p>
+                                        <h4>DEPOZIT</h4>
+                                        <p></p>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <p>
+                                        </p>
+                                        <h5>DAE</h5>
+                                        <h3 class="text-secondary">9.93%</h3>
+                                        <p></p>
+                                    </div>
+                                    <div class="col-md-3 ">
+                                        <a target="_blank" href="https://imfs.ro/simulator-credit/?tip=imobiliar" class="btn btn-warning align-middle">Negociaza dobanda</a>
+                                    </div>
+                                    <hr>
+                                    <div class="col-md-3">
+                                        <p>
+                                            <a target="_blank" href="https://www.bcr.ro"><img src="https://imfs.ro/wp-content/uploads/2021/04/BCR.svg.png" width="180" alt=""></a>
+                                        </p>
+                                        <h4>DEPOZIT</h4>
+                                        <p></p>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <p>
+                                        </p>
+                                        <h5>DAE</h5>
+                                        <h3 class="text-secondary">8.26%</h3>
+                                        <p></p>
+                                    </div>
+                                    <div class="col-md-3 ">
+                                        <a target="_blank" href="https://imfs.ro/simulator-credit/?tip=imobiliar" class="btn btn-warning align-middle">Negociaza dobanda</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-md-6">
-                                <p>
-                                    </p><h5>DAE</h5>
-                                    <h3 class="text-secondary">9.30%</h3>
-                                <p></p>
-                        </div>
-                        <div class="col-md-3 ">
-                            <a target="_blank" href="https://imfs.ro/simulator-credit/?tip=imobiliar" class="btn btn-warning align-middle">Negociaza dobanda</a>
-                        </div>
-                        <hr>
-                        <div class="col-md-3">
-                        <p>
-                            <a target="_blank" href="https://www.otpbank.ro/ro"><img src="https://imfs.ro/wp-content/uploads/2021/04/OTP_Bank_Rom%C3%A2nia.svg.png" width="180" alt=""></a>
-                            </p><h4>DEPOZIT</h4>
-                        <p></p>
-                        </div>
-                        <div class="col-md-6">
-                                <p>
-                                    </p><h5>DAE</h5>
-                                    <h3 class="text-secondary">9.24%</h3>
-                                <p></p>
-                        </div>
-                        <div class="col-md-3 ">
-                            <a target="_blank" href="https://imfs.ro/simulator-credit/?tip=imobiliar" class="btn btn-warning align-middle">Negociaza dobanda</a>
-                        </div>
-                        <hr>
-                        <div class="col-md-3">
-                        <p>
-                            <a target="_blank" href="https://www.brd.ro"><img src="https://imfs.ro/wp-content/uploads/2021/04/logo-mic.png" width="180" alt=""></a>
-                            </p><h4>DEPOZIT</h4>
-                        <p></p>
-                        </div>
-                        <div class="col-md-6">
-                                <p>
-                                    </p><h5>DAE</h5>
-                                    <h3 class="text-secondary">9.93%</h3>
-                                <p></p>
-                        </div>
-                        <div class="col-md-3 ">
-                            <a target="_blank" href="https://imfs.ro/simulator-credit/?tip=imobiliar" class="btn btn-warning align-middle">Negociaza dobanda</a>
-                        </div>
-                        <hr>
-                        <div class="col-md-3">
-                        <p>
-                            <a target="_blank" href="https://www.bcr.ro"><img src="https://imfs.ro/wp-content/uploads/2021/04/BCR.svg.png" width="180" alt=""></a>
-                            </p><h4>DEPOZIT</h4>
-                        <p></p>
-                        </div>
-                        <div class="col-md-6">
-                                <p>
-                                    </p><h5>DAE</h5>
-                                    <h3 class="text-secondary">8.26%</h3>
-                                <p></p>
-                        </div>
-                        <div class="col-md-3 ">
-                            <a target="_blank" href="https://imfs.ro/simulator-credit/?tip=imobiliar" class="btn btn-warning align-middle">Negociaza dobanda</a>
-                        </div>
-                   </div>
+
+
+                    </div>
                 </div>
+
             </div>
-
-
+            <!-- Container-fluid Ends-->
         </div>
+        <!-- footer start-->
+        <?php include('partial/footer.php') ?>
     </div>
-
-</div>
-      <!-- Container-fluid Ends-->
-    </div>
-    <!-- footer start-->
-    <?php include('partial/footer.php') ?>
-  </div>
 </div>
 <?php include('partial/scripts.php') ?>
 <!-- Plugins JS start-->
@@ -145,6 +175,6 @@
 <script src="assets/js/animation/wow/wow.min.js"></script>
 <!-- Plugins JS Ends-->
 <script>
-  new WOW().init();
+    new WOW().init();
 </script>
 <?php include('partial/footer-end.php') ?>
