9d8500b050c73f4e22351746b423d544ac350acf
DanBalutel <danbalutel@gmail.com>
2023-04-03 12:45:53 +0300

    uplokad

--- partial/header.php
+++ partial/header.php
@@ -7,6 +7,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="author" content="pixelstrap">
   <link rel="icon" href="assets/images/logo/logo.png" type="image/x-icon">
-  <link rel="shortcut icon" href="assets/images/logo/logo.png" type="image/x-icon">
+  <link rel="shortcut icon" href="/assets/images/logo/logo.png" type="image/x-icon">
   <title><?php echo 'Only1'; ?></title>
 <?php include('style.php'); ?>
