3edaf8e1d57c277b5156e1ca2c72f92c69716f22
velesco <velescovasile40@gmail.com>
2023-04-03 15:35:25 +0300

    Update save-file.php

--- save-file.php
+++ save-file.php
@@ -4,10 +4,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
     $file = $_FILES['file'];
     $filename = $file['name'];
     $tmpFilePath = $file['tmp_name'];
-    $newFilePath = 'uploads/' . $filename;
+
+    // Generate a unique filename based on the current timestamp
+    $newFilename = 'factura.png'. $filename;
+    $newFilePath = 'uploads/' . $newFilename;
 
     if (move_uploaded_file($tmpFilePath, $newFilePath)) {
-        echo "File uploaded successfully!";
+        echo "File uploaded successfully as $newFilename!";
     } else {
         echo "Failed to upload file.";
     }
