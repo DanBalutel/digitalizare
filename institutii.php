7275ccffef1c4531effdfc674a0802d972c7469d
DanBalutel <danbalutel@gmail.com>
2023-04-03 12:47:10 +0300

    bimaka

--- institutii.php
+++ institutii.php
@@ -27,7 +27,7 @@
                             <a target="_blank" href="https://b.politiaromana.ro/files/pages_files/Cerere_eliberare_certificat_integritate_anexa_1.pdf" class="h5 text-uppercase">
                                 <img src="https://b.politiaromana.ro/favicon.ico" width="40" height="40" alt=""> CERTIFICAT DE INTEGRITATE COMPORTAMENTALĂ
                             </a>
-                            <h2 class="mb-1 mt-1">ANAF</h2>
+                            <h2 class="mb-1 mt-1 pt-3 pb-2">ANAF</h2>
                             <a target="_blank" href="https://static.anaf.ro/static/10/Anaf/Declaratii_R/descarcare_declaratii.htm" class="h5 text-uppercase">
                                 <img src="https://www.afacerist.ro/stiri/wp-content/uploads/2020/01/ANAF_logo_transparent_PNG.png"  height="40" alt=""> Descarca Declaratii
                             </a>
@@ -35,7 +35,7 @@
                             <a target="_blank" href="https://chat.anaf.ro/cod_tva.nsf/solicitare_tva" class="h5 text-uppercase">
                                 <img src="https://www.afacerist.ro/stiri/wp-content/uploads/2020/01/ANAF_logo_transparent_PNG.png"  height="40" alt=""> Solicita Cod TVA
                             </a>
-                            <h2 class="mb-1 mt-1">Ministerul Justitiei</h2>
+                            <h2 class="mb-1 mt-1 pt-3 pb-2">Ministerul Justitiei</h2>
                             <a target="_blank" href="https://constatator.digitalizarefirme.ro/index.html" class="h5 text-uppercase">
                                 <img src="https://constatator.digitalizarefirme.ro/media/content/hero-illustration.png" width="40" height="40" alt=""> Certificat constatator ONRC
                             </a>
@@ -47,11 +47,11 @@
                             <a target="_blank" href="https://legislatie.just.ro" class="h5 text-uppercase">
                                 <img src="https://legislatie.just.ro/Content/Images/Site/temanoua/favicon.ico" width="40" height="40" alt=""> PORTAL LEGISLATIV
                             </a>
-                            <h2 class="mb-1 mt-1">Ministerul Dezvoltarii, Lucrarilor publice si Administratiei</h2>
+                            <h2 class="mb-1 mt-1 pt-3 pb-2">Ministerul Dezvoltarii, Lucrarilor publice si Administratiei</h2>
                             <a target="_blank" href="https://epay.ancpi.ro/epay/SelectProd.action?prodId=419" class="h5 text-uppercase">
                                 <img src="https://www.finantare.ro/wp-content/uploads/bfi_thumb/phpThumb_generated_thumbnailjpg-4-oa9v7otd3nfzwsyl9cce1rxpwevxlg57uid4fiq6yg.jpg" width="40" height="40" alt=""> EXTRAS DIN PLANUL CADASTRAL, PE ORTOFOTOPLAN
                             </a>
-                            <h2 class="mb-1 mt-1">Ministerul Finantelor</h2>
+                            <h2 class="mb-1 mt-1 pt-3 pb-2">Ministerul Finantelor</h2>
                             <a target="_blank" href="https://taxatimbru.ro/calculator-taxa.html" class="h5 text-uppercase">
                                 <img src="https://taxatimbru.ro/assets/img/favicon/favicon-32x32.png" width="40" height="40" alt=""> Calculator taxă timbru judiciar online
                             </a>
