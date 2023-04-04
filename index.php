272b3345e5521b2f58a6db2e55faf23f0fd67b12
DanBalutel <danbalutel@gmail.com>
2023-04-04 17:47:17 +0300

    test

--- index.php
+++ index.php
@@ -284,6 +284,46 @@
 
 
 
+          <div class="col-sm-12">
+            <div class="card">
+              <div class="card-header">
+                <h2 style="color:#00CCFF">Asociați/acționari și persoane autorizate - conexiuni cu alte firme</h2>
+                <h4 style="color:#00CCFF">Data ultimei actualizări: 02.03.2023</h4>
+
+              </div>
+              <div class="card-block row">
+                <div class="col-sm-12 col-lg-12 col-xl-12">
+                  <div class="table-responsive">
+                    <table class="table table-responsive-sm">
+                      <thead>
+                        <tr>
+                          <th scope="col">Numar Dosar</th>
+                          <th scope="col">Data</th>
+                          <th scope="col">Instanta</th>
+                          <th scope="col">%</th>
+                          <th scope="col">Firma</th>
+                          <th scope="col">Judet</th>
+                          <th scope="col">Localitate</th>
+
+                        </tr>
+                      </thead>
+                      <tbody id="dosareFirma">
+                      </tbody>
+                    </table>
+                  </div>
+
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+
+
+
+
+
+
           <h4 style="color: #00CCFF">Afisare sold/debit cont bancar in timp real</h4>
           <!-- banca transilvania -->
           <div class="col-xl-4 col-md-6 col-sm-12 box-col-6">
