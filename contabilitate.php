99ab669dc0d7cc65498c3862c834fe3bb62354c8
velesco <velescovasile40@gmail.com>
2023-04-04 16:32:00 +0300

    Update contabilitate.php

--- contabilitate.php
+++ contabilitate.php
@@ -1,5 +1,6 @@
 <?php include('partial/header.php') ?>
 
+<script src='https://unpkg.com/tesseract.js@v2.1.0/dist/tesseract.min.js'></script>
 
 
 <?php include('partial/loader.php') ?>
@@ -60,7 +61,6 @@
 <script src="assets/js/height-equal.js"></script>
 <script src="assets/js/animation/wow/wow.min.js"></script>
 <script src="assets/js/tooltip-init.js"></script>
-<script src='https://unpkg.com/tesseract.js@v2.1.0/dist/tesseract.min.js'></script>
 <script> 
     const obiect = JSON.parse(window.localStorage.dataObj);
     let imageURL;
