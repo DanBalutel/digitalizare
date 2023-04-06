1bb9fadd0cd64f03a46f5c1141e3a8cf3c1637ce
velesco <velescovasile40@gmail.com>
2023-04-06 14:30:22 +0300

    a

--- partial/footer.php
+++ partial/footer.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12 footer-copyright text-center">
-                <p class="mb-0">Copyright <?php echo date("Y"); ?> © Only1, toate drepturile rezevate.  </p>
+                <p class="mb-0">Copyright <?php echo date("Y"); ?> © AiPro, toate drepturile rezevate.  </p>
             </div>
         </div>
     </div>
