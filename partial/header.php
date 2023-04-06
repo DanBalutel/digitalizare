59a00ea6d700accdc5f3e4c54a68723f5177340a
velesco <velescovasile40@gmail.com>
2023-04-06 14:04:35 +0300

    a

--- partial/header.php
+++ partial/header.php
@@ -5,7 +5,7 @@
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="author" content="pixelstrap">
+  <meta name="author" content="Area4u">
   <link rel="icon" href="assets/images/logo/favicon.png" type="image/x-icon">
   <link rel="shortcut icon" href="/assets/images/logo/favicon.png" type="image/x-icon">
   <title><?php echo 'AiPro'; ?></title>
