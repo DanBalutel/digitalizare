3e7709f2e93120aeeed17c4f2021935aa168094a
DanBalutel <danbalutel@gmail.com>
2023-04-07 21:32:01 +0300

    ds

--- institutii.php
+++ institutii.php
@@ -16,7 +16,7 @@
             <?php include('partial/breadcrumb.php'); ?>
             <!-- Container-fluid starts-->
             <div class="container-fluid">
-                <h3><a href="#" id="demoPdfCreate">Click DEMO</a> - Completare automata documente - Transmitere automata solicitari Institutii</h3>
+                <h3><a href="#" id="demoPdfCreate">Click DEMO</a><br>- Completare automata documente<br>- Transmitere automata solicitari Institutii</h3>
                 <div class="card">
                     <div class="card-body">
                         <h2 class="mb-1 mt-1 pt-3 pb-2">Ministerul Afacerilor Interne</h2>
