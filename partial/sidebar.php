6bb2c2872bf8b965ff1098aa83d469c0d568753e
velesco <velescovasile40@gmail.com>
2023-04-07 14:27:50 +0300

    a

--- partial/sidebar.php
+++ partial/sidebar.php
@@ -261,7 +261,7 @@
                <li class="sidebar-list mb-3">
                   <a class="sidebar-link" type="button" data-bs-toggle="modal" data-bs-target="#oferta" data-bs-original-title="Oferta de pret" title="Oferta de pret">
                      <img class="rounded-circle float-start chat-user-img img-30 m-r-1" src="../assets/images/logo/favicon.png" alt="">
-                     <span>Cere de pret</span>
+                     <span>Oferta de pret AiPro</span>
                   </a>
                </li>
              
