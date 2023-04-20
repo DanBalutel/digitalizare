3bb85fcfe3c0451de761ad03913694db3708c729
DanBalutel <danbalutel@gmail.com>
2023-04-20 13:04:59 +0300

    padding

--- partial/topbar.php
+++ partial/topbar.php
@@ -17,7 +17,7 @@
       <div class="toggle-sidebar"><img class="img-fluid" src="/assets/images/logo/logo.png" width="50" alt=""></a></div>
     </div>
 
-    <div class="nav-left col-1 pull-right pr-3">
+    <div class="nav-left col-1 pull-right p-0">
       <ul class="nav-menus">
         <li class="onhover-dropdown text-center" style="font-size: 17px; color: #00CCFF; z-index: 3">
           Scan<br><i class="fa fa-camera" style="color: #00CCFF !important"></i>
