a79539163064de226131a33e9628792c3f3f5879
DanBalutel <danbalutel@gmail.com>
2023-05-18 13:31:11 +0300

    ds

--- assets/js/generatePdf.js
+++ assets/js/generatePdf.js
@@ -32,3 +32,39 @@ function generatePdf(fileName, fieldsObj) {
 
 
 }
+
+
+function generatePdfA6(fileName) {
+    // console.log(fieldsObj);
+    // loading
+    renderLoadingModal('<strong>Se încarcă datele</strong>, va rugam așteptați 😌🙏🏼.');
+
+    fetch('http://api.aipro.ro:3003/generate-pdf', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'cui': newdataObj.firma.cui,
+            'fileName': fileName,
+            'pdfPath': 'https://aipro.ro/assets/pdf/anexa6.pdf'
+            // 'fieldValues': fieldsObj
+        })
+
+
+    })
+        // SERVER RESPONSE
+        .then((result) => {
+            if (result.status != 200) { throw new Error("Bad Server Response"); }
+            console.log(result);
+            return result.text();
+        })
+        .then((response) => {
+            pdfLink = JSON.parse(response);
+            window.location.href = pdfLink.link;
+        })
+        .catch(error => console.log(error));
+
+
+
+}
