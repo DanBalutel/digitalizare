7d6ef0e8958674794cbc13049f8e00bcb9e7d7d2
velesco <velescovasile40@gmail.com>
2023-05-26 18:18:16 +0300

    Create oferta.php

---
+++ oferta.php
@@ -0,0 +1,32 @@
+<?php include('partial/header.php');?>
+
+<?php include('partial/loader.php'); ?>
+
+<div class="page-wrapper compact-wrapper" id="pageWrapper">
+    <!-- Page Header Start-->
+    <?php include('partial/topbar.php'); ?>
+    <!-- Page Header Ends -->
+    <!-- Page Body Start-->
+    <div class="page-body-wrapper">
+        <!-- Page Sidebar Start-->
+        <?php include('partial/sidebar.php');?>
+        <!-- Page Sidebar Ends-->
+        <div class="page-body">
+            <?php include('partial/breadcrumb.php'); ?>
+            <!-- Container-fluid starts-->
+            <div class="container-fluid">
+
+          </div>
+            <!-- Container-fluid Ends-->
+        </div>
+        
+        <?php include('partial/footer.php');?>
+    </div>
+</div>
+
+<?php include('partial/scripts.php'); ?>
+
+  <script src="assets/js/notify/index.js"></script>
+
+<script src="assets/js/height-equal.js"></script>
+<?php include('partial/footer-end.php'); ?>
