57eb982d7e2000ed4b3b45c12315dc3b9568bbbd
DanBalutel <danbalutel@gmail.com>
2023-08-11 11:57:47 +0300

    ds

--- bubai.php
+++ bubai.php
@@ -1,13 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body {
+            margin: 0;
+            /* Remove default margin */
+        }
+
+        iframe {
+            display: block;
+            /* iframes are inline by default */
+            height: 100vh;
+            /* Set height to 100% of the viewport height */
+            width: 100vw;
+            /* Set width to 100% of the viewport width */
+            border: none;
+            /* Remove default border */
+            background: lightyellow;
+            /* Just for styling */
+        }
+    </style>
 </head>
+
 <body>
-    
-<iframe src="https://gamma.app/embed/m9f66abq49b5eay" style="width: 99.5vw; max-width: 100%; height: 98.5vh" allow="fullscreen" title="Portofoliu UX design pentru Jora"></iframe>
+
+    <iframe src="https://gamma.app/embed/m9f66abq49b5eay" style="width: 99.5vw; max-width: 100%; height: 98.5vh" allow="fullscreen" title="Portofoliu UX design pentru Jora"></iframe>
 
 </body>
+
 </html>
