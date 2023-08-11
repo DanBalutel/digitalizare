7e7bf90c1019ac71712a2b386e7786c092106b4e
DanBalutel <danbalutel@gmail.com>
2023-08-11 11:52:27 +0300

    bubai

---
+++ bubai.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<iframe src="https://gamma.app/embed/m9f66abq49b5eay" style="width: 99.5vw; max-width: 100%; height: 98.5vh" allow="fullscreen" title="Portofoliu UX design pentru Jora"></iframe>
+
+</body>
+</html>
