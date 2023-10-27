9fdfaf6b0a1fb6587cb905753aba8baec356f4dc
velesco <velescovasile40@gmail.com>
2023-10-27 14:49:03 +0300

    Update upload_files.php

--- upload_files.php
+++ upload_files.php
@@ -2,14 +2,24 @@
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
     $file = $_FILES['file'];
-    $filename = $file['name'];
+    $filename = basename($file['name']); // Using basename() for security reasons
     $cui = isset($_POST['cui_file']) ? $_POST['cui_file'] : '';
+    
+    // Sanitize the cui value
+    $cui = preg_replace("/[^a-zA-Z0-9]/", "", $cui); 
+
     $tmpFilePath = $file['tmp_name'];
 
-    $newFilePath = 'uploads/'.$cui.'/' . $tmpFilePath;
+    // Create the directory if it doesn't exist
+    $directoryPath = 'uploads/' . $cui;
+    if (!is_dir($directoryPath)) {
+        mkdir($directoryPath, 0755, true);
+    }
+
+    $newFilePath = $directoryPath . '/' . $filename;
 
     if (move_uploaded_file($tmpFilePath, $newFilePath)) {
-        echo "File uploaded successfully as $newFilename!";
+        echo "File uploaded successfully as $newFilePath!";
     } else {
         echo "Failed to upload file.";
     }
