83bdd8930c02d1d5cf398f5f3a63cb6c8e397da4
velesco <velescovasile40@gmail.com>
2023-10-27 14:59:25 +0300

    Update upload_files.php

--- upload_files.php
+++ upload_files.php
@@ -1,14 +1,10 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
-    $file = $_FILES['file'];
-    $filename = basename($file['name']); // Using basename() for security reasons
     $cui = isset($_POST['cui_file']) ? $_POST['cui_file'] : '';
     
     // Sanitize the cui value
     $cui = preg_replace("/[^a-zA-Z0-9]/", "", $cui); 
-
-    $tmpFilePath = $file['tmp_name'];
 
     // Create the directory if it doesn't exist
     $directoryPath = 'uploads/' . $cui;
@@ -16,12 +12,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
         mkdir($directoryPath, 0755, true);
     }
 
-    $newFilePath = $directoryPath . '/' . $filename;
+    // Loop through each file and handle them
+    foreach ($_FILES['file']['name'] as $index => $filename) {
+        $filename = basename($filename); // Using basename() for security reasons
+        $tmpFilePath = $_FILES['file']['tmp_name'][$index];
+        $newFilePath = $directoryPath . '/' . $filename;
 
-    if (move_uploaded_file($tmpFilePath, $newFilePath)) {
-        echo "File uploaded successfully as $newFilePath!";
-    } else {
-        echo "Failed to upload file.";
+        if (move_uploaded_file($tmpFilePath, $newFilePath)) {
+            echo "File uploaded successfully as $newFilePath!<br>";
+        } else {
+            echo "Failed to upload file: $filename.<br>";
+        }
     }
 } else {
     echo "Invalid request.";
