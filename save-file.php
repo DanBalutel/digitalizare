dedc1f448b28e1b1fa2adb0f23de996e5c91d3bf
velesco <velescovasile40@gmail.com>
2023-11-03 13:11:45 +0200

    Update save-file.php

--- save-file.php
+++ save-file.php
@@ -1,22 +1,22 @@
 <?php
 
-// if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
-//     $file = $_FILES['file'];
-//     $filename = $file['name'];
-//     $cui = isset($_POST['cui_file']) ? $_POST['cui_file'] : '';
-//     $tmpFilePath = $file['tmp_name'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
+    $file = $_FILES['file'];
+    $filename = $file['name'];
+    $cui = isset($_POST['cui_file']) ? $_POST['cui_file'] : '';
+    $tmpFilePath = $file['tmp_name'];
 
-//     // Generate a unique filename based on the current timestamp
-//     $newFilename = $cui.'_factura.png';
-//     $newFilePath = 'uploads/' . $newFilename;
+    // Generate a unique filename based on the current timestamp
+    $newFilename = $cui.'_factura.png';
+    $newFilePath = 'uploads/' . $newFilename;
 
-//     if (move_uploaded_file($tmpFilePath, $newFilePath)) {
-//         echo "File uploaded successfully as $newFilename!";
-//     } else {
-//         echo "Failed to upload file.";
-//     }
-// } else {
-//     echo "Invalid request.";
-// }
+    if (move_uploaded_file($tmpFilePath, $newFilePath)) {
+        echo "File uploaded successfully as $newFilename!";
+    } else {
+        echo "Failed to upload file.";
+    }
+} else {
+    echo "Invalid request.";
+}
 
 ?>
