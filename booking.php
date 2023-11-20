96ce4cbf5f7c5a72638cf55ce689d617d1da761c
velesco <velescovasile40@gmail.com>
2023-11-20 16:08:19 +0200

    Create booking.php

---
+++ booking.php
@@ -0,0 +1,53 @@
+<?php include('partial/header.php') ?>
+<?php include('partial/loader.php') ?>
+<!-- page-wrapper Start-->
+<div class="page-wrapper compact-wrapper" id="pageWrapper">
+    <!-- Page Header Start-->
+    <?php include('partial/topbar.php') ?>
+    <!-- Page Header Ends-->
+    <!-- Page Body Start-->
+    <div class="page-body-wrapper">
+        <!-- Page Sidebar Start-->
+        <?php include('partial/sidebar.php') ?>
+        <!-- Page Sidebar Ends-->
+        <div class="page-body">
+
+            <?php
+            $title = 'Booking';
+            include('partial/breadcrumb.php')
+            ?>
+            <!-- Container-fluid starts-->
+            <div class="container-fluid">
+                
+                <iframe src="https://www.booking.com/index.ro.html?label=gen173nr-1BCAEoggI46AdIM1gEaMABiAEBmAEguAEXyAEV2AEB6AEBiAIBqAIDuALK0O2qBsACAdICJGNiOGY3YjBmLWIyOTQtNDI5ZC1iNmFkLTE0YjZiYWJmZGRlZNgCBeACAQ&sid=aeca1262c3e2faa58590dfa72262f258&keep_landing=1&sb_price_type=total&" frameborder="0"></iframe>
+            </div>
+
+        </div>
+        <!-- footer start-->
+        <?php include('partial/footer.php') ?>
+    </div>
+</div>
+<?php include('partial/scripts.php') ?>
+<!-- Plugins JS start-->
+
+<script src="assets/js/chart/apex-chart/moment.min.js"></script>
+<script src="assets/js/notify/bootstrap-notify.min.js"></script>
+<script src="assets/js/dashboard/default.js"></script>
+<script src="assets/js/notify/index.js"></script>
+<script src="assets/js/typeahead/handlebars.js"></script>
+<script src="assets/js/typeahead/typeahead.bundle.js"></script>
+<script src="assets/js/typeahead/typeahead.custom.js"></script>
+<script src="assets/js/typeahead-search/handlebars.js"></script>
+<script src="assets/js/typeahead-search/typeahead-custom.js"></script>
+<script src="assets/js/height-equal.js"></script>
+<script src="assets/js/animation/wow/wow.min.js"></script>
+<script src="assets/js/chart/morris-chart/raphael.js"></script>
+<script src="assets/js/chart/morris-chart/morris.js"> </script>
+<script src="assets/js/chart/morris-chart/prettify.min.js"></script>
+<script src="assets/js/chart/morris-chart/morris-script.js"></script>
+<script src="assets/js/tooltip-init.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.0/xlsx.full.min.js"></script>
+
+</script>
+<?php include('partial/footer-end.php') ?>
