f0a28952dfe228cdba8945b427221217c0635941
velesco <velescovasile40@gmail.com>
2023-11-23 15:19:42 +0200

    Update monica.php

--- monica.php
+++ monica.php
@@ -83,7 +83,6 @@ async function checkIncercari() {
         const cui = window.localStorage.getItem('cui');
         const response = await fetch(`https://punctaj.ro/api/incercari/${cui}`);
         const attempts = await response.text();
-        console.log(response.json())
         console.log(response.text())
         console.log(attempts)
         if(attempts > 0) {
